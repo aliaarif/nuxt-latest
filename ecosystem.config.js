2893ce07d8db22dc155491053329b60abd12e699
Mohammed Aarif <ali.aliaarif@gmail.com>
2023-07-14 22:15:59 +0530

    Add ecosystem.config.js file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,11 +10,11 @@ module.exports = {
         // "production" is the environment name
         production: {
             user: 'web',
-            host: ['192.168.1.6'],
-            ref: 'origin/master',
-            repo: 'git@git-repo.git',
+            host: ['172.105.61.7'],
+            ref: 'origin/main',
+            repo: 'git@github.com:aliaarif/nuxt-latest.git',
             ssh_options: ['ForwardAgent=yes'],
-            path: '/var/www/warrenlee.tech',
+            path: '/var/nuxt-latest',
             'post-deploy': 'yarn && yarn build && pm2 startOrRestart ecosystem.config.js --env production'
         }
     }
